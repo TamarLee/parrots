929e7dffc2f10922d9b45df55736b579b55a52a6
DESKTOP-IVAB5BU\user <tmekhri@gmail.com>
2018-08-01 19:21:15 +0400

    hamburger menu

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <title>თუთიყუშები</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="{{ url('css/reset.css') }}">
     <link rel="icon" type="image/jpg" href="{{ url('image/t.jpg') }}">
     <link rel="stylesheet" href="{{ url('css/archyedt-bold.min.css') }}">
@@ -9,6 +10,8 @@
     <link rel="stylesheet" type="text/css" href="{{ url('css/bootstrap-grid.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ url('css/bootstrap-grid.min.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ url('css/mystyle.css') }}">
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <script type="text/javascript" src="{{ url('js/script.js') }}"></script>
 </head>
 <body>
 
@@ -23,7 +26,13 @@
     <header>
 
         <img src="{{ url('image/15.jpg') }}" alt="parrot">
-        <nav>
+
+        <div class="icon">
+          <div class="hamburger"></div>
+      </div>
+
+
+        <!-- <nav>
             <ul>
                 <li>მთავარი</li>
                 <li>თუთიყუშები</li>
@@ -32,7 +41,9 @@
                 <li>კონტაქტი</li>
             </ul>
         </nav>
-        <div class="clear"></div>
+        <div class="clear"></div> -->
     </header>
+    
 </body>
+
 </html>
