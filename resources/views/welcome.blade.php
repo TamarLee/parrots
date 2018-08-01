afff0e2fcc23814c5139083fd5935fb4ec61e8ee
DESKTOP-IVAB5BU\user <tmekhri@gmail.com>
2018-08-01 17:09:30 +0400

    Laravel basic

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>თუთიყუშები</title>
+    <link rel="stylesheet" type="text/css" href="{{ url('css/reset.css') }}">
+    <link rel="icon" type="image/jpg" href="{{ url('image/t.jpg') }}">
+    <link rel="stylesheet" href="{{ url('css/archyedt-bold.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ url('css/bootstrap.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ url('css/bootstrap-grid.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ url('css/bootstrap-grid.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ url('css/mystyle.css') }}">
+</head>
+<body>
+
+    <!-- pre loader -->
+    <div  class="preloader">
+
+        <div class="sk-spinner sk-spinner-pulse"></div>
+
+    </div>
+
+
+    <header>
+
+        <img src="{{ url('image/15.jpg') }}" alt="parrot">
+        <nav>
+            <ul>
+                <li>მთავარი</li>
+                <li>თუთიყუშები</li>
+                <li>გალერეა</li>
+                <li>ბლოგი</li>
+                <li>კონტაქტი</li>
+            </ul>
+        </nav>
+        <div class="clear"></div>
+    </header>
+</body>
+</html>
